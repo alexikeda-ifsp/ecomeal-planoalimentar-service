c7bbd8e6fb312b1504dfe2444e1853f492cbcc52
alexikeda-ifsp <alex.ikeda@aluno.ifsp.edu.br>
2026-06-23 15:59:07 -0300

    Ajustes para comunicação com banco Postgres

--- plano-alimentar-api/main.py
+++ plano-alimentar-api/main.py
@@ -1,43 +1,65 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
 from database import select_operation
 from connection_api import create_connection
-from fastapi import HTTPException
-from fastapi.responses import JSONResponse
+
+
 app = FastAPI()
 
-@app.get("/planos")
-async def verifica_planos():
-    """
-    Verifica os Planos Alimentares existentes. Retorna uma
-    Lista de Objetos Plano Alimentar
-    """
-    client = create_connection()
-    if client is None:
+
+@app.get("/planos/")
+async def busca_planos():
+
+    connection = create_connection()
+
+    if connection is None:
         raise HTTPException(
             status_code=500,
-            detail="Erro na Conexão com o Banco de Dados."
+            detail="Erro na conexão com banco"
         )
-    planos_alimentar = select_operation(client,"plano_alimentar")
-    return JSONResponse(content=planos_alimentar, status_code=200)
+
+
+    planos = select_operation(
+        connection,
+        "plano_alimentar"
+    )
+
+
+    return JSONResponse(
+        content=planos,
+        status_code=200
+    )
+
+
 
 @app.get("/planos/{id}")
-async def verifica_plano(id):
-    """
-    Verifica um plano específico pelo seu ID, retornando um objeto
-    Plano Alimentar. Se não existir, retornar code 404.
-    """
-    client = create_connection()
-    if client is None:
+async def busca_plano(id: str):
+
+    connection = create_connection()
+
+    if connection is None:
         raise HTTPException(
             status_code=500,
-            detail="Erro na Conexão com o Banco de Dados."
+            detail="Erro na conexão com banco"
         )
-    try:
-        plano_alimentar = select_operation(client, "plano_alimentar",id)
-    except Exception as e:
-        if e.code=="22P02":
-            raise HTTPException(status_code=400, detail="ID deve estar no formato UUID. ID fornecido é Inválido")
-    if not plano_alimentar:
-        raise HTTPException(status_code=404,detail="Plano Alimentar não encontrado.") 
-    return JSONResponse(content=plano_alimentar,status_code=200)
 
+
+    plano = select_operation(
+        connection,
+        "plano_alimentar",
+        id
+    )
+
+
+    if not plano:
+        raise HTTPException(
+            status_code=404,
+            detail="Plano alimentar não encontrado"
+        )
+
+
+    return JSONResponse(
+        content=plano,
+        status_code=200
+    )
--- refeicoes-api/main.py
+++ refeicoes-api/main.py
@@ -1,35 +1,69 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
 from database import select_operation
 from connection_api import create_connection
-from fastapi import HTTPException
-from fastapi.responses import JSONResponse
+
 
 app = FastAPI()
 
-@app.get("/refeicoes")
-async def verifica_refeicoes():
-    """
-    Retorna uma Lista de Objetos Refeicao. Se não houver, é retornado uma Lista vazia.
-    """
-    client = create_connection()
-    if client is None:
-        raise HTTPException(status_code=500, detail="Erro na Conexão com o Banco de Dados.")
-    refeicoes = select_operation(client,"refeicao")
-    return JSONResponse(content=refeicoes, status_code=200)
+
+
+@app.get("/refeicoes/")
+async def busca_refeicoes():
+
+    connection = create_connection()
+
+
+    if connection is None:
+        raise HTTPException(
+            status_code=500,
+            detail="Erro na conexão com banco"
+        )
+
+
+    refeicoes = select_operation(
+        connection,
+        "refeicao"
+    )
+
+
+    return JSONResponse(
+        content=refeicoes,
+        status_code=200
+    )
+
+
+
 @app.get("/refeicoes/{id}")
-async def verifica_refeicao(id:str):
-    """
-    Busca e retorna um Objeto Refeicao pelo seu ID. Se não encontrado,
-    retorna 404.
-    """
-    client = create_connection()
-    if client is None:
-        raise HTTPException(status_code=500, detail="Falha na Conexão com o Banco de Dados")
-    try:
-        refeicao = select_operation(client,"refeicao",id)
-    except Exception as e:
-        if e.code=="22P02":
-            raise HTTPException(status_code=400,detail="ID deve estar no formato UUID. ID fornecido é Inválido")
+async def busca_refeicao(id:str):
+
+    connection = create_connection()
+
+
+    if connection is None:
+        raise HTTPException(
+            status_code=500,
+            detail="Erro na conexão com banco"
+        )
+
+
+    refeicao = select_operation(
+        connection,
+        "refeicao",
+        id
+    )
+
+
     if not refeicao:
-        raise HTTPException(status_code=404,detail="Refeição não encontrada.")
-    return JSONResponse(content=refeicao,status_code=200)
+
+        raise HTTPException(
+            status_code=404,
+            detail="Refeição não encontrada"
+        )
+
+
+    return JSONResponse(
+        content=refeicao,
+        status_code=200
+    )
